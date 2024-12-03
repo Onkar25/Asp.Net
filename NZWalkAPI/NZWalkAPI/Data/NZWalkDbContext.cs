9e872424fa46bbdb721c432ab4b6aba7bf60a5cf
Onkar Murkar <onkar.murkar@outlook.com>
2024-12-04 02:54:36 +0530

    Added NZWalkAPI Project

---
+++ NZWalkAPI/NZWalkAPI/Data/NZWalkDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using NZWalkAPI.Models.Domain;
+
+namespace NZWalkAPI.Data
+{
+	public class NZWalkDbContext : DbContext
+	{
+		public NZWalkDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+		{
+
+		}
+
+		public DbSet<Difficulty> Difficulties { get; set; }
+
+		public DbSet<Region> Regions { get; set; }
+
+		public DbSet<Walk> Walks { get; set; }
+
+
+
+	}
+}
+
