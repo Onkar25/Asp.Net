3479725632d199a62af62ab7e863f0f9a0d12042
Onkar Murkar <onkar.murkar@outlook.com>
2025-05-08 00:50:09 +0530

    upload the code

--- Angular/DatingApp/client/src/app/messages/messages.component.ts
+++ Angular/DatingApp/client/src/app/messages/messages.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from '../_services/message.service';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [FormsModule,ButtonsModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
